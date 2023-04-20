c27e51cdfac33ab828f2b56fde61033ba112297f
atran032 <ant032@ucsd.edu>
2023-04-20 09:03:26 -0700

    .

--- ArrayTests.java
+++ ArrayTests.java
@@ -24,8 +24,27 @@ public class ArrayTests {
 	}
 
   @Test 
+	public void testReverseInPlace3() {
+    int[] input1 = { 3, 4, 5, 4, 3 };
+    ArrayExamples.reverseInPlace(input1);
+    assertArrayEquals(new int[]{ 3, 4, 5, 4, 3 }, input1);
+	}
+
+  @Test 
 	public void testReversed2() {
     int[] input1 = { 1, 2, 3, 4, 5 };
     assertArrayEquals(new int[]{ 5, 4, 3, 2, 1 }, ArrayExamples.reversed(input1));
+	}
+
+  @Test 
+	public void testReversed3() {
+    int[] input1 = { 1, 2, 3, 4, 0 };
+    assertArrayEquals(new int[]{ 0, 4, 3, 2, 1 }, ArrayExamples.reversed(input1));
+	}
+
+  @Test 
+	public void testReversed4() {
+    int[] input1 = { 0, 0, 0, 0 };
+    assertArrayEquals(new int[]{ 0, 0, 0, 0 }, ArrayExamples.reversed(input1));
 	}
 }
