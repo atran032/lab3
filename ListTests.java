e04fb7c8a700d215c753b7aaaa69f2a14bb9f5f4
atran032 <ant032@ucsd.edu>
2023-04-22 00:13:57 -0700

    .

--- ListTests.java
+++ ListTests.java
@@ -1,11 +1,25 @@
 import static org.junit.Assert.*;
 import org.junit.*;
+import java.util.Arrays;
+import java.util.List;
 
 public class ListTests {
     @Test
-    public void testMerge() {
-        String[] input1 = {"hello", "hi"};
-        String[] input2 = {"hey", "Greetings"};
-        assertEquals({"hello", "hi", "hey", "Greetings"}, ListExamples.merge(input1, input2));
+    public void testMerge1() {
+        List<String> list1 = Arrays.asList("a", "c");
+        List<String> list2 = Arrays.asList("b", "c");
+        assertArrayEquals(new String[]{"a", "b", "c", "d"}, ListExamples.merge(list1, list2).toArray());
+    }
+    @Test
+    public void testMerge2() {
+        List<String> list1 = Arrays.asList("a", "b");
+        List<String> list2 = Arrays.asList("c", "d");
+        assertArrayEquals(new String[]{"a", "b", "c", "d"}, ListExamples.merge(list1, list2).toArray());
+    }
+    @Test
+    public void testMerge3() {
+        List<String> list1 = Arrays.asList("a", "c", "e");
+        List<String> list2 = Arrays.asList("b", "d", "f");
+        assertArrayEquals(new String[]{"a", "b", "c", "d", "e", }, ListExamples.merge(list1, list2).toArray());
     }
 }
